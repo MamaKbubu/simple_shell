98e71d86f6207e80e43950574167e7045ea070ae
MamaKbubu <maboelatk@gmail.com>
2022-12-13 13:51:19 -0800

    update

--- README.md
+++ README.md
@@ -5,5 +5,3 @@ THE GATES OF SHELL
 This is an ALX collaboration project on Shell. We were tasked with creating a simple UNIX Command Intepreter. Our shell shall be called "..."
 
 To invoke this shell, compile the simple_shell.c and run the resulting executable. 
-
-
--- simple_shell.c
+++ simple_shell.c
@@ -5,33 +5,34 @@
 #include<sys/wait.h>
 
 #define MAX_LINE 80
-#define MAX_ARGS MAX_LINE/2 + 1
-
-int main(void) 
+#define MAX_ARGS MAX_LINE(2 + 1)
+int main(void)
 {
-    char *args[MAX_ARGS];
-    char line[MAX_LINE];
-    int should_run = 1;
-
-    while(should_run) {
-        printf("Kat_Tshego > ");
-        fflush(stdout);
-
-        // read input
-        fgets(line, MAX_LINE, stdin);
-
-        // tokenize input
-        args[0] = strtok(line, " \n");
-        int i = 0;
-        while(args[i] != NULL) {
-            args[++i] = strtok(NULL, " \n");
+	char *args[MAX_ARGS];
+	char line[MAX_LINE];
+	int should_run = 1;
+	
+	while (should_run)
+	{
+		printf("Kat_Tshego > ");
+		fflush(stdout);
+		// read input //
+		fgets(gline, MAX_LINE, stdin);
+		
+		// tokenize input //
+		args[0] = strtok(line, " \n")
+		int i = 0;
+		while (args[i] != NULL)
+		{
+			args[++i] = strtok(NULL, " \n");
         }
-
-        // run command
-        pid_t pid = fork();
-        if(pid == 0) {
-            execvp(args[0], args);
-            exit(1);
+		// run command //
+		pid_t pid = fork();
+		
+		if (pid == 0)
+		{
+			execvp(args[0], args);
+			exit(1);
 	}
     }
 }
