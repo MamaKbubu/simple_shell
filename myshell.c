0d73484ea8a623cc5fa7c1efb3dd6ce4c2955b3a
Kat-Devs <katlegophele95@gmail.com>
2022-12-19 22:12:27 -0800

    update for shell mode

--- myshell.c
+++ myshell.c
@@ -1,66 +1,180 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/wait.h>
+#include <string.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 
-#define MAX_LEN 1000
-#define MAX_ARGS 10
+#define BUFFERSIZE 256
 
-/* function declarations */
-void parse_command(char *cmd_line, char **args);
-int execute_command(char **args);
+//Function prototypes
+int processinput(char *input); //Processing the input
+int parseinput(char *input, char **args); //Parsing the input into a list of arguments
+int execcommand(char **args); //Executing the command
+int execute(char **args); //Executing the command and handling errors
+int builtin_command(char **args); //Handling the built-in commands
 
-int main() {
-    char cmd_line[MAX_LEN];
-    char *args[MAX_ARGS];
-
-    while (1) {
-        printf(">> ");
-        fgets(cmd_line, MAX_LEN, stdin);
-        parse_command(cmd_line, args);
-        if (execute_command(args) == -1)
-            break;
-    }
-
-    return 0;
-}
-
-void parse_command(char *cmd_line, char **args) {
-    int i = 0;
-    args[i] = strtok(cmd_line, " \n");
-    while (args[i] != NULL) {
-        args[++i] = strtok(NULL, " \n");
-    }
-}
-
-int execute_command(char **args) {
-    int status;
-
-    if (strcmp(args[0], "env") == 0) {
-        /* display environment */
-        char *env = getenv("PATH");
-        printf("PATH=%s\n", env);
-    } else if (strcmp(args[0], "exit") == 0) {
-        /* exit shell */
-        return -1;
-    } else {
-        /* execute command */
-        pid_t pid = fork();
-        if (pid < 0) {
-            printf("Fork failed\n");
-            return 1;
-        } else if (pid == 0) {
-            /* child process */
-            if (execvp(args[0], args) < 0) {
-                printf("Exec failed\n");
-            }
-            exit(0);
-        } else {
-            /* parent process */
-            while (wait(&status) != pid);
+int main(int argc, char *argv[])
+{
+    char *input; //Used to store the input
+    size_t inputsize = BUFFERSIZE; //Used to set the size of the buffer
+    int status; //Used to store the status code of the processes
+    
+    //Check if the program is running in interactive mode
+    if (isatty(fileno(stdin)))
+    {
+        //If the program is running in interactive mode,
+        //continuously prompt the user for input
+        while (1) 
+        {
+            printf("unix>");
+            input = (char *)malloc(BUFFERSIZE * sizeof(char));
+            getline(&input, &inputsize, stdin);
+            status = processinput(input);
+            
+            //Free the allocated memory
+            free(input);
+            
+            //If the user wants to exit, break from the loop
+            if (status == 0)
+                break;
         }
     }
+    else
+    {
+        //If the program is running in non-interactive mode,
+        //read the input from stdin
+        while (1)
+        {
+            input = (char *)malloc(BUFFERSIZE * sizeof(char));
+            getline(&input, &inputsize, stdin);
+            status = processinput(input);
+            
+            //Free the allocated memory
+            free(input);
+            
+            //If the user wants to exit, break from the loop
+            if (status == 0)
+                break;
+        }
+    }
+    
     return 0;
+}
+
+//Process the input
+int processinput(char *input)
+{
+    int status; //Used to store the status code of the processes
+    char **args; //Used to store the list of arguments
+    
+    //Parse the input into a list of arguments
+    status = parseinput(input, args);
+    
+    //Execute the command
+    status = execcommand(args);
+    
+    //Return the status code of the executed process
+    return status;
+}
+
+//Parse the input into a list of arguments
+int parseinput(char *input, char **args)
+{
+    int i = 0; //Used as an index
+    
+    //Split the input into a list of arguments
+    args[i] = strtok(input, " \t\r\n\a");
+    while (args[i] != NULL)
+    {
+        i++;
+        args[i] = strtok(NULL, " \t\r\n\a");
+    }
+    
+    return 0;
+}
+
+//Execute the command
+int execcommand(char **args)
+{
+    int status; //Used to store the status code of the processes
+    
+    //Execute the command and handle errors
+    status = execute(args);
+    
+    return status;
+}
+
+//Execute the command and handle errors
+int execute(char **args)
+{
+    int status; //Used to store the status code of the processes
+    
+    //Check if the command is a built-in command
+    status = builtin_command(args);
+    if (status != 0)
+    {
+        //If the command is not a built-in command,
+        //fork a new process to execute the command
+        pid_t pid, wpid;
+        pid = fork();
+        
+        //Check if the fork was successful
+        if (pid == 0)
+        {
+            //If the fork was successful,
+            //execute the command
+            if (execvp(args[0], args) == -1)
+            {
+                perror("Error");
+            }
+            exit(EXIT_FAILURE);
+        }
+        else if (pid < 0)
+        {
+            //If the fork failed,
+            //print an error message
+            perror("Error");
+        }
+        else
+        {
+            //Wait for the new process to finish
+            do
+            {
+                wpid = waitpid(pid, &status, WUNTRACED);
+            } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+        }
+    }
+    
+    return status;
+}
+
+//Handle the built-in commands
+int builtin_command(char **args)
+{
+    int status = 1; //Used to store the status code of the processes
+    
+    //Check if the command is the exit built-in
+    if (strcmp(args[0], "exit") == 0)
+    {
+        //If the command is the exit built-in,
+        //set the status code to 0 and exit
+        status = 0;
+        exit(EXIT_SUCCESS);
+    }
+    //Check if the command is the env built-in
+    else if (strcmp(args[0], "env") == 0)
+    {
+        //If the command is the env built-in,
+        //print the current environment
+        status = 0;
+        char **env = environ;
+        while (*env)
+        {
+            printf("%s\n", *env);
+            env++;
+        }
+    }
+    
+    return status;
 }
